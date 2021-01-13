b64d6c3198bf6d2d7c1107a37f0ff424a727dfc9
nicolasreymond <n.reymond.12@gmail.com>
2021-01-13 10:45:08 +0100

    [ADD] Create a file with all needed data.

---
+++ docker/api/object/condef/conparam_template.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @remark	Remplir correctement les constantes ci-dessous en fonction de votre base de données
+ */
+
+/*
+ * @brief Connection constants
+ */
+define('EDB_DBTYPE', "XXX");
+define('EDB_DBNAME', "XXX");
+define('EDB_HOST',   "XXX");
+define('EDB_PORT',   "XXX");
+define('EDB_USER',   "XXX");
+define('EDB_PASS',   "XXX");
