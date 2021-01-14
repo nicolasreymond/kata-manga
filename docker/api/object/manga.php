188485a14ea9d621048dc7a34221972bd7b76e68
nicolasreymond <n.reymond.12@gmail.com>
2021-01-14 20:29:14 +0100

    [WIP] Work on the switch for the api

--- docker/api/object/manga.php
+++ docker/api/object/manga.php
@@ -25,3 +25,54 @@
         $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
         return $result;
     }
+
+    $path_info = $_SERVER["PATH_INFO"];
+    switch ($request_method) {
+        
+        case 'GET':
+            $matched = array();
+            if (preg_match('|/manga/(\d+)|', $path_info, $matched)) {                     # /manga/5
+                send_json_response(
+                    getManga($matched[1])
+                );
+            } elseif (preg_match('|/manga/findByAuthor/(\w+)|', $path_info, $matched)) {  # /manga/findByAuthor/oda,miyazaki
+                send_json_response(
+                    findMangaByAuthors(explode(",", $matched[1]))
+                );
+            } elseif (preg_match('|/manga/findByAuthor$|', $path_info, $matched)) {  # /manga/findByAuthor?author=oda,miyazaki
+                send_json_response(
+                    findMangaByAuthors(explode(",", $_GET["author"]))
+                );
+            } 
+            elseif (!empty($_GET["id"])) {
+                $id=intval($_GET["id"]);
+                getManga($id);
+            } else {
+                send_json_response(
+                    getMangas()
+                );
+            }
+            break;
+
+        case 'POST':
+            // Ajouter un manga
+            AddManga();
+            break;
+
+        case 'DELETE':
+            // Supprimer un manga
+            $id = intval($_GET["id"]);
+            deleteManga($id);
+            break;
+
+        case 'PUT':
+            // Modifier un manga
+            updateManga();
+            break;
+
+        default:
+            // Invalid Request Method
+            header("HTTP/1.0 405 Method Not Allowed");
+            break;
+
+    }
