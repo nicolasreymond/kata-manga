0c3e74f3b615aa5822f4a0e3287058c4f0e5db12
nicolasreymond <n.reymond.12@gmail.com>
2021-01-18 17:34:32 +0100

    [WIP] Start of the frontend  - Add a php service on docker.  - Add index.php and manga.php pages

---
+++ docker/web/manga.php
@@ -0,0 +1,32 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+?>
+
+<!doctype html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8">
+  <title>Kata-manga</title>
+  <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+  <div class="head">
+    <header>
+      <div class=""><a href="index.php">Kata-manga</a></div>
+        
+    </header>
+
+    <div class="head-content">
+        <div class="menu">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="manga.php">Manga</a></li>
+                <li><a href="API">API</a></li>
+            </ul>
+        </div>  
+    </div>
