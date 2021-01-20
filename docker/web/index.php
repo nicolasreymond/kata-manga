17a1f76b79f2fcc7fb08e10d160c3f58476d5add
nicolasreymond <n.reymond.12@gmail.com>
2021-01-20 18:20:42 +0100

    [RFA] remove unuseful code

--- docker/web/index.php
+++ docker/web/index.php
@@ -1,53 +1,3 @@
-<?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-// require("api_function.php");
-// construct the query with our apikey and the query we want to make
-$endpoint = 'http://localhost:1337/api/object/api.php/manga/';
-// setup curl to make a call to the endpoint
-
-
-    function getJoke($firstName, $lastName)
-    {
-        //http://api.icndb.com/jokes/random
-        
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, "http://api.icndb.com/jokes/random?firstName=" . $firstName . "&lastName=" . $lastName);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_HTTPGET, 1);
-        
-        $JokeJSON = curl_exec($ch);
-        if ($JokeJSON == false) {
-            die("cURL Error: " . curl_error($ch));
-        }
-        
-        $JokeObj = json_decode($JokeJSON, true);
-        return $JokeObj['value']['joke'];
-    }
-    
-    function getManga($id)
-    {
-        //http://api.icndb.com/jokes/random
-        
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, "http://web:8080/api/object/api.php/manga/" . $id);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_HTTPGET, 1);
-        
-        $MangaJSON = curl_exec($ch);
-        echo $MangaJSON;
-        if ($MangaJSON == false) {
-            die("cURL Error: " . curl_error($ch));
-        }
-        
-        $JokeObj = json_decode($MangaJSON, true);
-        echo $JokeObj;
-        return $JokeObj[0]['id'];
-    }
-
-?>
-
 <!doctype html>
 <html lang="fr">
 
@@ -77,8 +27,7 @@ $endpoint = 'http://localhost:1337/api/object/api.php/manga/';
 ?>
 <div class="grille">
   <h1 class="">Voici quelques test d'api</h1>
-<?=getJoke("Nicolas", "Reymond")?>
-<br>
-<?=getManga(7)?>
+
+
 
 </div>
--- docker/web/manga.php
+++ docker/web/manga.php
@@ -1,6 +1,9 @@
 <?php
 require("api_function.php");
  
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 
 ?>
@@ -12,41 +15,7 @@ require("api_function.php");
   <meta charset="UTF-8">
   <title>Kata-manga</title>
   <link rel="stylesheet" href="styles/style.css">
-  <style>
-
-    table, td, th {
-      border: 1px solid black;
-    }
-
-  table {
-    width: 100%;
-    border-collapse: collapse;
-  }
-  ul {
-  list-style-type: none;
-  margin: 0;
-  padding: 0;
-  overflow: hidden;
-  background-color: #333;
-}
-
-li {
-  float: left;
-}
-
-li a {
-  display: block;
-  color: white;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-}
-
-/* Change the link color to #111 (black) on hover */
-li a:hover {
-  background-color: #111;
-}
-  </style>
+  
 </head>
 
 <body>
