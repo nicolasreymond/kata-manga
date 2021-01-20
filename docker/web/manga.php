259855cd417562d8dc3c6fc7a4ff6ebdd8cc5b9e
nicolasreymond <n.reymond.12@gmail.com>
2021-01-20 16:25:27 +0100

    [RFA] Beautify the <table>

--- docker/web/manga.php
+++ docker/web/manga.php
@@ -11,7 +11,42 @@ error_reporting(E_ALL);
 <head>
   <meta charset="UTF-8">
   <title>Kata-manga</title>
-  <link rel="stylesheet" href="styles.css">
+  <link rel="stylesheet" href="styles/style.css">
+  <style>
+
+    table, td, th {
+      border: 1px solid black;
+    }
+
+  table {
+    width: 100%;
+    border-collapse: collapse;
+  }
+  ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+/* Change the link color to #111 (black) on hover */
+li a:hover {
+  background-color: #111;
+}
+  </style>
 </head>
 
 <body>
